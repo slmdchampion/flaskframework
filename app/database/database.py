1d463918e5d20d5ad6cfee3d588877863e5bd979
Robert Joseph <bob.joseph@gmx.com>
2023-05-29 20:50:30 -0600

    added ORM model

--- app/database/database.py
+++ app/database/database.py
@@ -1,8 +1,30 @@
 from sqlalchemy import text, create_engine, MetaData
 from app.database.base import sa
 
+# with app.app_context():
+#     db.reflect()
 
+# class User:
+#     __table__ = db.metadatas["auth"].tables["user"]
+
+#  insertdata = mybase.table.insert().values(name="angela")
+#  db.session.execute(insertdata) 
+#  db.session.commit()
+# 
+# query = mybase.table.select().where(mybase.table.c.name == "angela")
+# result = db.session.execute(query)
+class Table(sa.Model):
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
+    
 class Database:
+    # table=sa.Table("table", sa.metadata, autoload_with=sa.engine)
+
+    def __init__(self) -> None:
+        #reflect existing tables
+        self.table=sa.Table("table", sa.metadata, autoload_with=sa.engine)
+
     def get_test_table(self):
         result = sa.session.execute(text("SELECT * FROM `test`.`table`"))
         return result
+    
--- app/section1/routes.py
+++ app/section1/routes.py
@@ -1,11 +1,20 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from app.database.database import Database
+from app.database.database import Database, Table
+from app.database.base import sa
+
 
 index_blueprint = Blueprint('index', __name__, template_folder='templates', static_folder='static')
-mybase = Database()
 
+# uses SQL directly to query the database
 @index_blueprint.route("/")
 def index():
+    mybase=Database()
     result=mybase.get_test_table()
     return render_template('index.html', title='Home', table_data=result)
+
+# uses the ORM to query the database
+@index_blueprint.route("/page1")
+def page1():
+    result=sa.session.scalars(sa.select(Table)).all()
+    return render_template('page1.html', title='Page1', table_data=result)
  
